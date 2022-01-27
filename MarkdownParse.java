5d24c63a99f81b53bd99bf96582a6cf033d958cb
m1ma0314 <m1ma@ucsd.edu>
2022-01-27 13:54:29 -0800

    2MoreFilesAdded
    add condition to check if correct link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,12 +12,13 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+
             System.out.println(currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);//the second argument is for where to start research, if not provided, search start from index 0
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             //toReturn.add(markdown.substring(openParen + 1, closeParen));
-            if (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!'){
+            if (nextCloseBracket + 1 == openParen && (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!')){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
